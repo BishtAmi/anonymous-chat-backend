9156bba17940eec883b392f52e85484a109462b6
Amit_Bisht <singhbishtamit2@gmail.com>
2025-03-02 23:08:52 +0530

    feat: added PORT for deployment in render

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => handleConnection(ws, wss));
-
-server.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
   console.log("WebSocket server running on ws://localhost:8080");
 });
